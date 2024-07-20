b6ef5dda0a728e93a8c29652cd550b572d75ab2d
Abbas mkhzomi <amkhzomi@gmail.com>
2024-07-21 00:10:35 +0330

    create following migration and model

---
+++ app/Models/Following.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Following extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'following_id',
+    ];
+
+    /**
+     * Get the user that owns the Following
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the Followed User
+     *
+     * @return BelongsTo
+     */
+    public function following(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
